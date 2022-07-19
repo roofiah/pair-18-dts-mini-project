882b5d66b50f00aa961471abb10d1be87463de72
adawiyah <rofiatul.adawiyah2@gmail.com>
2022-07-19 19:40:51 +0700

    try slider movie list

--- src/containers/home/listMovies/PopularMovies.js
+++ src/containers/home/listMovies/PopularMovies.js
@@ -1,10 +1,16 @@
-import { ImageList, ImageListItem } from '@mui/material';
-import React, { useEffect, useState } from 'react'
-import MovieCard from '../../../components/MovieCard';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y } from "swiper";
 import apiUrl from '../../../utils/apiUrl';
 import './ListMovies.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 const PopularMovies = () => {
+    const navigate = useNavigate()
     const [movies, setMovies] = useState([]);
 
     const fetchPopular = async () => {
@@ -21,24 +27,44 @@ const PopularMovies = () => {
         fetchPopular()
     }, []);
 
+    const handleClick = (id) => {
+        navigate(`/movie/${id}`)
+    }
+
     return (
         <div className='container'>
             <h3 className='title-list'>Popular</h3>
             {movies ?
-                <ImageList sx={{
-                    gridAutoFlow: "column",
-                    gridTemplateColumns: "repeat(auto-fill,minmax(160px,1fr)) !important",
-                    gridAutoColumns: "minmax(160px, 1fr)"
-                }}>
-                    {movies.map((data) => (
-                        <ImageListItem>
-                            <MovieCard movie={data}></MovieCard>
-                        </ImageListItem>
-                    ))}
-                </ImageList>
+                <Swiper
+                    modules={[Navigation, Pagination, A11y]}
+                    navigation
+                    speed={300}
+                    slidesPerView={5}
+                    loop
+                    className="swiper-container"
+                >
+                    <div className="imgWrapper">
+                        {movies.map((movie) => {
+                            return (
+                                <SwiperSlide
+                                    className="swiper-slide"
+                                    key={movie.id}>
+                                    <div
+                                        className="imageContainer"
+                                        onClick={() => handleClick(movie.id)}
+                                    >
+                                        <img src={`${BASE_IMAGE_URL}${movie.backdrop_path}`} alt="poster" />
+                                        <div className="imgTitle">{movie.original_title}</div>
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
+                    </div>
+                </Swiper>
                 :
                 <div>No Movie yet...</div>}
         </div>
     )
 }
 export default PopularMovies
+
